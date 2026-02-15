87be1e1811a5582e4d1e17f9447b896329e3fe68
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2026-02-15 22:17:09 +0530

    scrollview unoptimised way

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,57 @@
 import ShoppingListItem from '../components/shopping-list-item';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TextInput, ScrollView } from 'react-native';
 import { theme } from '../theme';
-import { Link } from 'expo-router';
+import { useState } from 'react';
+import * as Crypto from 'expo-crypto';
+
+type ShoppingList = {
+  id: string;
+  name: string;
+  // isCompleted: boolean;
+};
+
+const newId = () => Crypto.randomUUID();
+
+const initialList: ShoppingList[] = [
+  { id: newId(), name: 'Coffee' },
+  { id: newId(), name: 'Tea' },
+  { id: newId(), name: 'Green Tea' },
+  { id: newId(), name: 'Coffee' },
+  { id: newId(), name: 'Green Tea' },
+  { id: newId(), name: 'Coffee' },
+  { id: newId(), name: 'Green Tea' },
+  { id: newId(), name: 'Coffee' },
+  { id: newId(), name: 'Green Tea' },
+  { id: newId(), name: 'Coffee' },
+  { id: newId(), name: 'Green Tea' },
+];
 
 export default function App() {
+  const [value, setValue] = useState('');
+  const [shoppingList, setSetShoppingList] = useState<ShoppingList[]>(initialList);
+  const handleSubmit = () => {
+    const newShoppingList = [{ id: newId(), name: value }, ...shoppingList];
+    setSetShoppingList(newShoppingList);
+    setValue('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Link href="/counter" style={{ textAlign: 'center', marginBottom: 18 }}>
-        Go to Counter
-      </Link>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Green Tea" isCompleted />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}>
+      <TextInput
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={setValue}
+        style={styles.textInput}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map(({ name, id }) => (
+        <ShoppingListItem name={name} key={id} />
+      ))}
+    </ScrollView>
   );
 }
 
@@ -20,6 +59,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: 'center',
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    backgroundColor: theme.colorWhite,
   },
 });
